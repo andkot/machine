1af489a3ebb34c8cfe6f6545e453bae2bc584c87
vroman10 <venchak.201130@gmail.com>
2021-07-25 22:27:24 +0300

    task_1 is redone

--- task_1/image_collector.py
+++ task_1/image_collector.py
@@ -1,20 +1,54 @@
 from pdf2image import convert_from_path
+from matplotlib import pyplot 
 import sys
 import os
+import argparse
 
 
-def convert(file, path):
-    pages = convert_from_path(file)
-    counter = 1
-    for page in pages:
-        myfile = path + "/" + str(counter) + '.ppm'
-        page.save(myfile)  
-        counter+=1
+# Create the parser
+my_parser = argparse.ArgumentParser()
 
+# Add the arguments
+my_parser.add_argument('-i',
+                        type=str,
+                        help='the path to pdf',
+                        required=True)
+my_parser.add_argument('-o',
+                        type=str,
+                        help='the path to save',
+                        default='.')
+my_parser.add_argument('-k',
+                        choices=['save', 'show'],
+                        help='не знаю что сказать, нахуй иди',
+                        default='save')
+my_parser.add_argument('-n',
+                        type=int,
+                        help='page number',
+                        default=1)
+# Execute the parse_args() method
+args = my_parser.parse_args()
 
-if len(sys.argv)>2:
-    file = sys.argv[1]
-    outputDir = sys.argv[2]
-    if not os.path.exists(outputDir):
-            os.makedirs(outputDir)
-    convert(sys.argv[1], sys.argv[2])
+input_path = args.i
+output_path = args.o
+key = args.k
+num = args.n
+
+print(input_path)
+print(output_path)
+print(key)
+
+def convert(input_path, output_path, key, num):
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    pages = convert_from_path(input_path)
+    if key == 'save':
+        counter = 1
+        for page in pages:
+            myfile = output_path + "/" + str(counter) + '.ppm'
+            page.save(myfile)  
+            counter+=1
+    elif key == 'show':
+        pyplot.imshow(pages[num])
+        pyplot.show()
+
+convert(input_path, output_path, key, num)
